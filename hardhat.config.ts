45fec2a474850d638f29d5f0c435d2a784862c79
Roy <toledoroy@gmail.com>
2022-03-10 08:26:57 -0800

    Working Environment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -25,11 +25,19 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    hardhat: {
     },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      // gas: 2100000,
+      // gasPrice: 8000000000
+      // gasPrice: 10000000000,
+    },
+    // ropsten: {
+    //   url: process.env.ROPSTEN_URL || "",
+    //   accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    // },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
