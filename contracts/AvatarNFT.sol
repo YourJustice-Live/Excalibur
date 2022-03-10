cb343e83babc290a24cd4cc70c5f0d21ab108521
Roy <toledoroy@gmail.com>
2022-03-10 13:47:12 -0800

    Main config & Non-transferable

--- contracts/AvatarNFT.sol
+++ contracts/AvatarNFT.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";  //Individual Metadata URI Storage Functions
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+//Interfaces
+import "./interfaces/IConfig.sol";
 
 
 /**
@@ -18,11 +20,52 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  */
 contract AvatarNFT is ERC721URIStorage, Ownable {
 
+    address private _CONFIG;    //Configuration Contract
+
     /**
 	 * Constructor
 	 */
-    constructor() ERC721("Avatar", "AVATAR") {
+    constructor(address config) ERC721("Avatar", "AVATAR") {
+        //Set Protocol's Config Address
+        _CONFIG = config;
+    }
 
+    /**
+     * Get Configurations Contract Address
+     */
+    function getConfig() public view returns (address) {
+        return _CONFIG;
+    }
+
+    /**
+     * Set Configurations Contract Address
+     */
+    function setConfig(address config) public onlyOwner {
+        //Validate Contract's Designation
+        require(keccak256(abi.encodePacked(IConfig(config).role())) == keccak256(abi.encodePacked("YJConfig")), "Invalid Config Contract");
+        //Set
+        _CONFIG = config;
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view override returns (address) {
+        return IConfig(getConfig()).owner();
+    }
+
+    /**
+     * @dev Transfer Rules
+     */
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal virtual override(ERC721) {
+        super._beforeTokenTransfer(from, to, tokenId);
+        require(
+            _msgSender() == owner()
+            || from == address(0)   //Minting
+            // || to == address(0)     //Burning
+            ,
+            "Sorry, Assets are non-transferable"
+        );
     }
     
 }
