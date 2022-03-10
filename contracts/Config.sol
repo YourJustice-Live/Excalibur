8bb5d5ec34606e10627e7bf29e292af349415d5d
Roy <toledoroy@gmail.com>
2022-03-10 13:35:12 -0800

    Main Protocol Config & Ownership

---
+++ contracts/Config.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * Global Configuration Contract
+ * - He who owns the config owns the protocol
+ */
+contract Config is Ownable {
+
+    // Arbitrary contract designation signature
+    string public constant role = "YJConfig";
+
+}
