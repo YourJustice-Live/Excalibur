b151f8e704feb95dce92d4bc860951f0a9021165
Roy <toledoroy@gmail.com>
2022-03-19 21:24:06 -0700

    Jurisdiction Interface

---
+++ contracts/interfaces/IJurisdiction.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+interface IJurisdiction {
+
+    /// Join a role in current jurisdiction
+    function join() external;
+
+    /// Leave Role in current jurisdiction
+    function leave() external;
+
+    /// Assign Someone to a Role
+    function roleAssign(address account, string calldata role) external;
+
+    /// Create a new Role
+    function roleCreate(address account, string calldata role) external;
+
+    
+    /*** EVENTS ***/
+
+    // event roleAdded(address account, string role);        // Role Added Event? ... Transfer events should cover this...
+
+}
