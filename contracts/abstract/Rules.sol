adaddd8e954e43ff6e35e46d590d2355dc4cf0b8
Roy <toledoroy@gmail.com>
2022-03-23 12:45:14 -0700

    Basic Rule Structure

--- contracts/abstract/Rules.sol
+++ contracts/abstract/Rules.sol
@@ -13,30 +13,43 @@ abstract contract Rules {
     
     //--- Storage
 
-    // Effect Object (Changes to Reputation By Type)
-    struct Effect {
-        uint8 professional;
-        uint8 social;
-        uint8 personal;
-    }
-
     // Rule Object
     struct Rule {
-        string name;
-        string uri;
+        // string name;
+        // string uri;
         //eventId: 1, 
         uint256 eventId;
         //text: "The founder of the project violated the contract, but this did not lead to the loss or freezing of funds for investors.", //Description For Humans
         string text;    
         // condition: "Investor funds were not frozen nor lost.",
         string condition;  
+        string uri;
         // effect: { //Reputation Change
         //     profiessional:-2,
         //     social: -4
         // }
-        Effect[3] effects;
+        // Effect[3] effects;
+        Effects effects;
     }
     
+    // Effect Object (Changes to Reputation By Type)
+    struct Effects {
+        // int8 professional;
+        // int8 social;
+        // int8 personal;
+        Effect professional;
+        Effect social;
+        Effect personal;
+    }
+    // Effect Structure
+    struct Effect {
+        // value: 5
+        int8 value;
+        // Direction: -
+        bool direction;
+        // Confidence/Strictness: [?]
+    }
+
     mapping(uint256 => Rule) private _rules;
 
     //--- Events
