7c15615d94a770e630fcf61c1f37c2cf9267d035
Roy <toledoroy@gmail.com>
2022-03-28 22:21:31 -0700

    [WIP] Rules

--- contracts/abstract/Rules.sol
+++ contracts/abstract/Rules.sol
@@ -1,8 +1,13 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2;    //https://docs.soliditylang.org/en/v0.5.2/abi-spec.html?highlight=abiencoderv2
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "./Opinions.sol";
 import "../interfaces/IRules.sol";
+import "../interfaces/IActionRepo.sol";
+
 
 /**
  * Rules Contract
@@ -10,21 +15,28 @@ import "../interfaces/IRules.sol";
  * - [TODO] Hold & Serve Rules
  * - [TODO] Event: Rule Added / Removed / Updated (can rules be removed?)
  */
-abstract contract Rules is IRules {
+// abstract contract Rules is IRules, Opinions {
+contract Rules is IRules, Opinions {
     
     //--- Storage
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _ruleIds;
 
     // Rule Object
     struct Rule {
         // string name;
         // string uri;
         //eventId: 1, 
-        uint256 eventId;
+        // uint256 about;    //About What (Token URI +? Contract Address)
+        bytes32 about;    //About What (Action's GUID)
+
         //text: "The founder of the project violated the contract, but this did not lead to the loss or freezing of funds for investors.", //Description For Humans
-        string text;     // is this needed?
+        // string text;
         // condition: "Investor funds were not frozen nor lost.",
-        string condition;  
-        string uri;
+        // string condition;  
+        string uri;     //Test & Conditions
+        
         // effect: { //Reputation Change
         //     profiessional:-2,
         //     social: -4
@@ -36,14 +48,15 @@ abstract contract Rules is IRules {
     
     // Effect Object (Changes to Reputation By Type)
     struct Effects {
-        // int8 professional;
-        // int8 social;
-        // int8 personal;
-        Effect environment;
-        Effect personal;
-        Effect social;
-        Effect professional;
+        int8 professional;
+        int8 social;
+        int8 personal;
+        // Effect environment;
+        // Effect personal;
+        // Effect social;
+        // Effect professional;
     }
+    /* Maybe?
     // Effect Structure
     struct Effect {
         // value: 5
@@ -52,19 +65,41 @@ abstract contract Rules is IRules {
         bool direction;
         // Confidence/Strictness: [?]
     }
+    */
 
-    mapping(uint256 => Rule) private _rules;
+    mapping(uint256 => Rule) internal _rules;
 
 
     //--- Functions
 
     // constructor() { }
 
+    /// Get Rule
+    function ruleGet(uint256 id) public view returns (Rule memory) {
+        return _rules[id];
+    }
+
     /// Add Rule
-    //ruleAdd()
+    function ruleAdd(address actionRepo, bytes32 actionGUID, Rule memory rule) public {
+        //TODO: Validate Caller's Permissions
+
+        _ruleAdd(actionRepo, actionGUID, rule);
+    }
+
+    /// Add Rule
+    function _ruleAdd(address actionRepo, bytes32 actionGUID, Rule memory rule) internal {
+        //TODO: Verify Action ID
+        // IActionRepo(actionRepo)
+
+
+        _ruleIds.increment();
+        uint256 newItemId = _ruleIds.current();
+        _rules[newItemId] = rule;
+    }
 
     /// Remove Rule
-    //ruleRemove()
-
+    function _ruleRemove(uint256 id) internal {
+        delete _rules[id];
+    }
 
 }
