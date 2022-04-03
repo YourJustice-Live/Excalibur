d8d3c57dcbc0e90c5f25396f69c977ee8daae6b6
Roy <toledoroy@gmail.com>
2022-04-03 12:43:10 -0700

    CommonYJ Upgradable

---
+++ contracts/abstract/CommonYJUpgradable.sol
@@ -0,0 +1,51 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "../interfaces/ICommonYJ.sol";
+import "../interfaces/IHub.sol";
+import "../libraries/DataTypes.sol";
+
+/**
+ * Common Protocol Functions
+ */
+abstract contract CommonYJUpgradable is ICommonYJ, OwnableUpgradeable {
+    
+    //--- Storage
+
+    // address internal _HUB;    //Hub Contract
+    IHub internal _HUB;    //Hub Contract
+    
+
+    //--- Functions
+
+    /// Initializer
+    function __CommonYJ_init(address hub) internal onlyInitializing {
+        // __Ownable_init();    //No Need
+        //Set Protocol's Config Address
+        _setHub(hub);
+    }
+
+    /// Inherit owner from Protocol's config
+    function owner() public view override(ICommonYJ, OwnableUpgradeable) returns (address) {
+        return _HUB.owner();
+    }
+
+    /// Set Hub Contract
+    function _setHub(address config) internal {
+        //Validate Contract's Designation
+        require(keccak256(abi.encodePacked(IHub(config).role())) == keccak256(abi.encodePacked("YJHub")), "Invalid Hub Contract");
+        //TODO: Check for ERC165 Interface
+        //Set
+        _HUB = IHub(config);
+    }
+
+    /// Set Hub Contract
+    function _getHub() internal view returns(address) {
+        return address(_HUB);
+    }
+    
+}
