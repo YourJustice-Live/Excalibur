32ed958999b4be92eaf9e54cc386516e3feb5a40
Roy <toledoroy@gmail.com>
2022-04-05 17:19:41 -0700

    Solidity Optimized

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,7 +23,22 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  
+  // solidity: "0.8.4",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+
   networks: {
     hardhat: {
     },
@@ -47,6 +62,7 @@ const config: HardhatUserConfig = {
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
+
 };
 
 export default config;
