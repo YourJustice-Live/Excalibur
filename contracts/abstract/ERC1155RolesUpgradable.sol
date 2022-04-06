622cf566a327374f0c18c59bfeb8f5b605fc74e9
Roy <toledoroy@gmail.com>
2022-04-05 17:19:27 -0700

    [REFAC] Role Func. instead of Role Modifier

--- contracts/abstract/ERC1155RolesUpgradable.sol
+++ contracts/abstract/ERC1155RolesUpgradable.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
+// import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
@@ -36,7 +36,8 @@ abstract contract ERC1155RolesUpgradable is IERC1155Roles, ERC1155GUIDUpgradable
         _;
     }
     
-    /// Validate that account hold one of the role in Array
+    /* CANCELLED
+    /// [TEST] Validate that account hold one of the role in Array
     modifier onlyRoles(string[] calldata roles) {
         bool hasRole;
         for (uint256 i = 0; i < roles.length; ++i) {
@@ -45,11 +46,14 @@ abstract contract ERC1155RolesUpgradable is IERC1155Roles, ERC1155GUIDUpgradable
         require(hasRole, "ROLE:INVALID_PERMISSION");
         _;
     }
-    /// Validate that account hold one of the role in Array
+
+    /// Validate that account hold one of the role in Array //Only works when the role is a parameter
     modifier onlyRole(string calldata role) {
         require(roleHas(_msgSender(), role), "ROLE:INVALID_PERMISSION");
         _;
     }
+    */
+
     //--- Functions
 
    /**
@@ -75,22 +79,24 @@ abstract contract ERC1155RolesUpgradable is IERC1155Roles, ERC1155GUIDUpgradable
         // return (balanceOf(account, _roleToId(_stringToBytes32(role))) > 0);
     }
 
-    /// Join a role in current jurisdiction
-    // function join() external override {
-    //     _GUIDAssign(_msgSender(), _stringToBytes32("member"));
-    // }
-
-    // /// Leave Role in current jurisdiction
-    // function leave() external override {
-    //     _GUIDRemove(_msgSender(), _stringToBytes32("member"));
-    // }
+    /// [TEST] Has Any of These Roles
+    function rolesHas(address account, string[] calldata roles) public view returns (bool) {
+        // bool hasRole;
+        for (uint256 i = 0; i < roles.length; ++i) {
+            if(roleHas(account, roles[i])){
+                // hasRole = true;
+                return true;
+            } 
+        }
+        // return hasRole;
+        return false;
+    }
 
     /// Assign Someone Else to a Role
     function _roleAssign(address account, string memory role) internal roleExists(role) {
         _GUIDAssign(account, _stringToBytes32(role));
         //TODO: Role Assigned Event?
     }
-
 
     /// Remove Someone Else from a Role
     function _roleRemove(address account, string memory role) internal roleExists(role) {
@@ -113,123 +119,5 @@ abstract contract ERC1155RolesUpgradable is IERC1155Roles, ERC1155GUIDUpgradable
     function _roleCreate(string memory role) internal returns (uint256) {
         return _GUIDMake(_stringToBytes32(role));
     }
-
-
-
-
-
-
-    /* SNIPPETS START */
-    
-    //--- Fractal DAO Access Control  https://github.com/fractal-framework/fractal-contracts/blob/93bc0e845a382673f3714e7df858e846d0f10b37/contracts/AccessControl.sol
-    /*
-    string public constant DAO_ROLE = "DAO_ROLE";
-    
-    mapping(string => RoleData) private _roles;
-    mapping(address => mapping(bytes4 => string[])) private _actionsToRoles;
-    
-
-    //TODO: Validate that account hold one of the role in Array
-    /// @notice Modifier that checks that an account has a specific role. Reverts
-    /// with a standardized message including the required role.
-    // modifier onlyRole(string memory role) {
-    // modifier onlyRole(array roles) {
-    //     _checkRole(role, msg.sender);
-    //     _;
-    // }
-    */
-
-    
-    //--- OZ  https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/AccessControl.sol
-
-    /*
-    struct RoleData {
-        mapping(address => bool) members;
-        bytes32 adminRole;
-    }
-    mapping(bytes32 => RoleData) private _roles;
-    bytes32 public constant DEFAULT_ADMIN_ROLE = 0x00;
-    */
-
-    /**
-     * @dev Modifier that checks that an account has a specific role. Reverts
-     * with a standardized message including the required role.
-     *
-     * The format of the revert reason is given by the following regular expression:
-     *
-     *  /^AccessControl: account (0x[0-9a-f]{40}) is missing role (0x[0-9a-f]{64})$/
-     *
-     * _Available since v4.1._
-     * /
-    modifier onlyRole(bytes32 role) {
-        _checkRole(role);
-        _;
-    }
-    
-    /**
-     * @dev Returns `true` if `account` has been granted `role`.
-     * /
-    function hasRole(bytes32 role, address account) public view virtual override returns (bool) {
-        return _roles[role].members[account];
-    }
-
-    /**
-     * @dev Revert with a standard message if `_msgSender()` is missing `role`.
-     * Overriding this function changes the behavior of the {onlyRole} modifier.
-     *
-     * Format of the revert message is described in {_checkRole}.
-     *
-     * _Available since v4.6._
-     * /
-    function _checkRole(bytes32 role) internal view virtual {
-        _checkRole(role, _msgSender());
-    }
-
-     /**
-     * @dev Revert with a standard message if `account` is missing `role`.
-     *
-     * The format of the revert reason is given by the following regular expression:
-     *
-     *  /^AccessControl: account (0x[0-9a-f]{40}) is missing role (0x[0-9a-f]{64})$/
-     * /
-    function _checkRole(bytes32 role, address account) internal view virtual {
-        if (!hasRole(role, account)) {
-            revert(
-                string(
-                    abi.encodePacked(
-                        "AccessControl: account ",
-                        Strings.toHexString(uint160(account), 20),
-                        " is missing role ",
-                        Strings.toHexString(uint256(role), 32)
-                    )
-                )
-            );
-        }
-    }
-
-
-
-    /**
-     * @dev See {IERC165-supportsInterface}.
-     * /
-    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
-        return interfaceId == type(IAccessControl).interfaceId || super.supportsInterface(interfaceId);
-    }
-
-
-    //https://gist.github.com/axic/ce82bdd1763c04ef8138c2b905985dab
-    library StringAsKey {
-        function convert(string key) returns (bytes32 ret) {
-            if (bytes(key).length > 32) {
-            throw;
-            }
-
-            assembly {
-            ret := mload(add(key, 32))
-            }
-        }
-        }
-    /* SNIPPETS END */
-
 
 }
