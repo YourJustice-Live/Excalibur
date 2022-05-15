db5ae3398afd2b9378dd9ac32c019fcbb9ae9d22
Roy <toledoroy@gmail.com>
2022-05-15 12:33:53 -0700

    Cleanup

--- contracts/AvatarNFT.sol
+++ contracts/AvatarNFT.sol
@@ -8,7 +8,7 @@ pragma solidity 0.8.4;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";  //Individual Metadata URI Storage Functions
 import "@openzeppelin/contracts/utils/Counters.sol";
 // import "./interfaces/IConfig.sol";
-import "./libraries/DataTypes.sol";
+// import "./libraries/DataTypes.sol";
 import "./interfaces/IAvatar.sol";
 import "./abstract/CommonYJ.sol";
 import "./abstract/Opinions.sol";
