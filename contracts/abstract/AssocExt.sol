0c68cd17bbdf2bffcbb1cf6af0872b9f2bef47a7
Roy <toledoroy@gmail.com>
2022-05-26 11:01:18 -0700

    Utilize an AssocRepo

---
+++ contracts/abstract/AssocExt.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+// import "hardhat/console.sol";
+
+import "../interfaces/IAssocRepo.sol";
+
+/**
+ * @title Generic Associations (to other contracts)
+ * @dev To Extend or Be Used by other contracts
+ * - Hold, Update & Serve Associations
+ */
+abstract contract AssocExt {
+    //AssocExt
+            
+    //--- Storage
+    IAssocRepo private _AssocRepo;
+
+    //--- Functions
+
+    //Set Assoc Repo
+    function _setAssocRepo(IAssocRepo assocRepo_) internal {
+        _AssocRepo = assocRepo_;
+    }
+
+    //Get Assoc Repo
+    function assocRepo() internal view returns (IAssocRepo) {
+        return _AssocRepo;
+    }
+
+
+}
