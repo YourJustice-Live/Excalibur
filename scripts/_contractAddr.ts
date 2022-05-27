f7ddeb6f1c7268285280ad0a2a7b750ce3df869d
Roy <toledoroy@gmail.com>
2022-05-27 13:51:05 -0700

    [DEV] Update Contracts Deployed to Rinkeby

--- scripts/_contractAddr.ts
+++ scripts/_contractAddr.ts
@@ -1,9 +1,9 @@
 //Track Addresses (Fill in present addresses to prevent new deplopyment)
 const contractAddr = {
     config:"0x14E5D5B68A41665E86225e6830a69bb2b5F6E484",  //V2.0
-    jurisdiction:"0x24a97995d48c1808A131eAc1655c70EC5961B12B",  //V2
-    case:"0xc62c829B8A95bfA8ca92B3D18Bc2dc93dded1D70",  //Case Instance //V2
-    hub:"0x25b1c6923a42F00028F878ff4D01B2030Cb69D75", //V3.1
+    jurisdiction:"0x80BC45E47e120120Cbeb829204523adddFc0224F",  //V2.1
+    case:"0x3C3107EfF84C46711C7426F2E505763854AD631F",  //V2.1
+    hub:"0x6f64EFf9F908Ac07FbEaE4b2BC48a849c301f683", //V3.2
     avatar:"0xDE2B0216CE0B83E7dcEb418d190901221A19C456",  //V1.2
     history:"0xe699f8dd6968F7a60786E846899CEf56154D3573", //V4.0
 };
