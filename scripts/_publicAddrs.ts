372ef146d7cfbb3c86fc034666ad39395c6dc12f
Roy <toledoroy@gmail.com>
2022-05-31 10:53:52 -0700

    Deployed a new AssocRepo

--- scripts/_publicAddrs.ts
+++ scripts/_publicAddrs.ts
@@ -1,6 +1,6 @@
 //Track Addresses (Fill in present addresses to user existing deplopyment)
 const publicAddr = {
-    assocRepo: "0x4784450ED6075EFA886437F5fC288eE9FEd9fba9", //V1.0
+    assocRepo: "0xfd5592B4604c5a422c507c4b116b51EE8e80E0C3", //V1.1
 };
 
 export default publicAddr;
