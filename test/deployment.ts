03c026a6d62c51e47425caf4c4d7bdcfd04e0515
Roy <toledoroy@gmail.com>
2022-05-30 19:12:15 -0700

    [TEST] Updatable Contracts Deployment

---
+++ test/deployment.ts
@@ -0,0 +1,157 @@
+import { expect } from "chai";
+import { Contract, ContractReceipt, Signer } from "ethers";
+import { ethers } from "hardhat";
+const {  upgrades } = require("hardhat");
+
+
+//Test Data
+// const ZERO_ADDR = '0x0000000000000000000000000000000000000000';
+// let test_uri = "ipfs://QmQxkoWcpFgMa7bCzxaANWtSt43J1iMgksjNnT4vM1Apd7"; //"TEST_URI";
+
+
+describe("Deployment", function () {
+    let jurisdictionContract: Contract;
+    let caseContract: Contract;
+    let hubContract: Contract;
+    let configContract: Contract;
+    let actionRepoContract: Contract;
+    let assocRepoContract: Contract;
+    let SoulUpgradable: Contract;
+    // let actionContract: Contract;
+    let oldHubContract: Contract;
+
+    //Addresses
+    let account1: Signer;
+    let account2: Signer;
+
+    before(async function () {
+
+        //Populate Accounts
+        [account1, account2] = await ethers.getSigners();
+
+        //--- ActionRepo
+        assocRepoContract = await ethers.getContractFactory("AssocRepo").then(res => res.deploy());
+        await assocRepoContract.deployed();
+       
+        //--- Config
+        configContract = await ethers.getContractFactory("Config").then(res => res.deploy());
+        await configContract.deployed();
+
+        //--- Jurisdiction Implementation
+        jurisdictionContract = await ethers.getContractFactory("JurisdictionUpgradable").then(res => res.deploy());
+        await jurisdictionContract.deployed();
+
+        //--- Case Implementation
+        caseContract = await ethers.getContractFactory("CaseUpgradable").then(res => res.deploy());
+        await caseContract.deployed();
+        
+        /* */  
+        //--- Hub Contract
+        // hubContract = await ethers.getContractFactory("Hub").then(res => res.deploy(
+        oldHubContract = await ethers.getContractFactory("Hub").then(res => res.deploy(
+            configContract.address, 
+            jurisdictionContract.address,
+            caseContract.address,
+        ));
+        // await hubContract.deployed();
+        await oldHubContract.deployed();
+        // console.log("Hub Address:", hubContract.address);
+        
+        
+        /*
+        //Deploy Avatar
+        avatarContract = await ethers.getContractFactory("AvatarNFT").then(res => res.deploy(hubContract.address));
+        //Set Avatar Contract to Hub
+        hubContract.setAssoc("avatar", avatarContract.address);
+        */
+
+        /*
+        //--- ActionRepo
+        actionRepoContract = await ethers.getContractFactory("ActionRepo").then(res => res.deploy(hubContract.address));
+        //Set Avatar Contract to Hub
+        hubContract.setAssoc("history", actionRepoContract.address);
+        */
+
+    });
+
+/* */
+    it("Should Deploy Upgradable Hub Contract", async function () {
+        //Deploy Avatar Upgradable
+        const HubUpgradable = await ethers.getContractFactory("HubUpgradable");
+        
+        // deploying new proxy
+        const proxyHub = await upgrades.deployProxy(HubUpgradable,
+            [
+                configContract.address, 
+                assocRepoContract.address, 
+                jurisdictionContract.address,
+                caseContract.address,
+            ],{
+            // https://docs.openzeppelin.com/upgrades-plugins/1.x/api-hardhat-upgrades#common-options
+            kind: "uups",
+            timeout: 120000
+        });
+        await proxyHub.deployed();
+        // console.log("HubUpgradable deployed to:", proxyHub.address);
+        hubContract = proxyHub;
+    });
+
+    it("Should Remember & Serve Config", async function () {
+        expect(await hubContract.getAssoc("config")).to.equal(configContract.address);
+    });
+
+    it("Should Change Hub", async function () {
+        oldHubContract.hubChange(hubContract.address);
+    });
+
+    it("Should Deploy Upgradable Soul Contract", async function () {
+        //Deploy Avatar Upgradable
+        const SoulUpgradable = await ethers.getContractFactory("SoulUpgradable");
+        // deploying new proxy
+        const proxyAvatar = await upgrades.deployProxy(SoulUpgradable,
+            [hubContract.address],{
+            // https://docs.openzeppelin.com/upgrades-plugins/1.x/api-hardhat-upgrades#common-options
+            kind: "uups",
+            timeout: 120000
+        });
+        await proxyAvatar.deployed();
+        this.avatarContract = proxyAvatar;
+        //Set Avatar Contract to Hub
+        hubContract.setAssoc("avatar", proxyAvatar.address);
+        // console.log("SoulUpgradable deployed to:", proxyAvatar.address);
+    });
+
+    it("Should Add Avatar", async function () {
+        await this.avatarContract.add("");
+    });
+
+    /* COPIED
+    it("Should Be Secure", async function () {
+        await expect(
+            hubContract.connect(account2).hubChange(hubContract2.address)
+          ).to.be.revertedWith("Ownable: caller is not the owner");
+    });
+
+    it("Should Remember & Serve Config", async function () {
+        expect(await hubContract.getConfig()).to.equal(configContract.address);
+    });
+
+    it("Should Move Children Contracts to a New Hub", async function () {
+        //Validate Configs
+        expect(await configContract.owner()).to.equal(this.addr1);
+        expect(await configContract2.owner()).to.equal(this.addr2);
+        //Validate Hub        
+        expect(await hubContract.owner()).to.equal(this.addr1);
+        expect(await hubContract2.owner()).to.equal(this.addr2);
+        //Check Before
+        expect(await avatarContract.owner()).to.equal(this.addr1);
+        expect(await actionContract.owner()).to.equal(this.addr1);
+        //Change Hub
+        hubContract.hubChange(hubContract2.address);
+        //Check After
+        expect(await avatarContract.owner()).to.equal(this.addr2);
+        expect(await actionContract.owner()).to.equal(this.addr2);
+    });
+    */
+    
+});
