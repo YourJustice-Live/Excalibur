e22c32ed9a52cf8e741115fc0ba9076507132f5f
Roy <toledoroy@gmail.com>
2022-06-01 09:45:02 -0700

    [DEPLOY] New deployment (test 0.3)

--- scripts/_contractAddr.ts
+++ scripts/_contractAddr.ts
@@ -1,10 +1,10 @@
 //Track Addresses (Fill in present addresses to prevent new deplopyment)
 const contractAddr = {
     config:"0x14E5D5B68A41665E86225e6830a69bb2b5F6E484",  //V2.0
-    jurisdiction:"0x80BC45E47e120120Cbeb829204523adddFc0224F",  //V2.1
-    case:"0x3C3107EfF84C46711C7426F2E505763854AD631F",  //V2.1
-    hub:"0xFAE2286855D6006F02c961CBf5413B2F19b31c04", //V4.0 (Proxy)        //Don't use this one
-    avatar:"0xc396BcDd78162b9A29a46550D38407454E4FC1B1",  //V2.3 (Proxy)
+    jurisdiction:"0xE5e14Eb9CFc4c266A844DD711938B3026970D6D1",  //V2.2
+    case:"0xAA9F16064f0F066A462c6702f4527aEf767A7700",  //V2.2
+    hub:"0xcdb0662dC5a446bc662607f44339ca8bD569DB97", //V4.1 (Proxy)
+    avatar:"0xfE0B5A71323f2aCA18643ad79B7bd7d916286898",  //V2.4 (Proxy)
     history:"0xe699f8dd6968F7a60786E846899CEf56154D3573", //V4.0
 };
 
