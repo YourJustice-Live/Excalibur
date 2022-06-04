580638f3f3adbd8658ffbf7cdf9c3e4adcac5b58
Roy <toledoroy@gmail.com>
2022-06-03 23:01:33 -0700

    [DEPLOY] Updated Dev Version of Jurisdiction Contract

--- scripts/_contractAddr.ts
+++ scripts/_contractAddr.ts
@@ -1,7 +1,7 @@
 //Track Addresses (Fill in present addresses to prevent new deplopyment)
 const contractAddr = {
     config:"0x14E5D5B68A41665E86225e6830a69bb2b5F6E484",  //V2.0
-    jurisdiction:"0xE5e14Eb9CFc4c266A844DD711938B3026970D6D1",  //V2.2
+    jurisdiction:"0xe52a833001f250fbD28fe3F93D61fa9fB0AB7484",  //V2.3
     case:"0x6Bc238B90E17c291e2159fdCcB4fBd94aF8fcfD7",  //V2.3
     hub:"0xcdb0662dC5a446bc662607f44339ca8bD569DB97", //V4.1 (Proxy)
     avatar:"0xfE0B5A71323f2aCA18643ad79B7bd7d916286898",  //V2.4 (Proxy)
