a1b98b111bdd83ca494440c72b1d1a8c3f57693c
Roy <toledoroy@gmail.com>
2022-06-09 15:45:06 -0700

    Library for handling Uint Arrays

---
+++ contracts/libraries/UintArray.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+/**
+ * @dev Basic Array Functionality
+ */
+library UintArray {
+
+    /// Remove Item From Array
+    function removeItem(uint256[] storage array, uint256 targetAddress) internal {
+        removeIndex(array, findIndex(array, targetAddress));
+    }
+    
+    /// Remove Item From Array
+    function removeIndex(uint256[] storage array, uint index) internal {
+        require(index < array.length, "UintArray:INDEX_OUT_OF_BOUNDS");
+        array[index] = array[array.length-1];
+        array.pop();
+    }
+
+    /// Find Item Index in Array
+    function findIndex(uint256[] storage array, uint256 value) internal view returns (uint256) {
+        for (uint256 i = 0; i < array.length; ++i) {
+            if(array[i] == value) return i;
+        }
+        revert("UintArray:ITEM_NOT_IN_ARRAY");
+    }
+
+}
