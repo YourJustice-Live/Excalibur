89a02afaf00eb8b73b1d05a7035e595dbdeefbde
Roy <toledoroy@gmail.com>
2022-06-10 10:48:15 -0700

    New Deployment for C & J

--- scripts/_contractAddr.ts
+++ scripts/_contractAddr.ts
@@ -1,8 +1,8 @@
 //Track Addresses (Fill in present addresses to prevent new deplopyment)
 const contractAddr = {
     config:"0x14E5D5B68A41665E86225e6830a69bb2b5F6E484",  //V2.0
-    jurisdiction:"0x90412734FA4225E50a28456BbF5914D2b8eE99c2",  //V2.7
-    case:"0x84d74C1c34C240DaeAA2031DbDBfF95C17D2d147",  //V2.4
+    jurisdiction:"0x5CC48aDB4FDB750BB862bB7fe2F3110f859CA7c6",  //V2.8
+    case:"0x03DE6Be8CFBD3e8fc9eD0f59838F8eb0189e5Dcb",  //V2.5
     hub:"0x997f2f4d8fA9a2d7688ae293Dc7Ab1da3f462701", //V4.2 (Proxy)
     avatar:"0xfE0B5A71323f2aCA18643ad79B7bd7d916286898",  //V2.4 (Proxy)
     history:"0xe699f8dd6968F7a60786E846899CEf56154D3573", //V4.0
