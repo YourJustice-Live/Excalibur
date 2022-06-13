4439bc7a3ebecc573a6929398cd8650a53c1cdf1
Roy <toledoroy@gmail.com>
2022-06-12 18:28:14 -0700

    Deployed New Protocol - 0.3 RC2

--- scripts/_contractAddr.ts
+++ scripts/_contractAddr.ts
@@ -3,9 +3,9 @@ const contractAddr = {
     config:"0xA888d7f44638AEEBA9048Ca08bCF83a23e9f58dc",  //D2.1
     jurisdiction:"0x6D9c0313Ab8D5C2b76711d201472E9d0F565d381",  //D2.9
     case:"0xded92D349f97f969e435c6e6b417B541874FC46a",  //D2.6
-    hub:"0x101F8810070D7A89E4e79BEB919b45717839C360", //D4.3 (Proxy)
-    avatar:"0x56e7D301070592d0f9A877bB885A10EB7eC713B0",  //D2.5 (Proxy)
-    history:"0xEf0bEf48C827349e87BF06D6c79655582713C9ce", //D4.1
+    hub:"0x3b549c05c88281AB0747DE54bA84Cd79B334d67e", //D4.4 (Proxy)
+    avatar:"0x99d9730803Ab1a2ec9695F68aC9a48342079096c",  //D2.6 (Proxy)
+    history:"0x0F5083439bf6da5b8dd343e6aDb80f6643D117eb", //D4.2
 };
 
 export default contractAddr;
