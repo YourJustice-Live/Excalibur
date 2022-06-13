fe5b1857b9b320394cd84999db71616af284a2ff
Roy <toledoroy@gmail.com>
2022-06-12 18:14:15 -0700

    Deployed OpenRepo V1.0

--- scripts/_publicAddrs.ts
+++ scripts/_publicAddrs.ts
@@ -1,6 +1,7 @@
 //Track Addresses (Fill in present addresses to user existing deplopyment)
 const publicAddr = {
     assocRepo: "0xfd5592B4604c5a422c507c4b116b51EE8e80E0C3", //V1.1
+    openRepo: "0xfD066cbf6Ed5cAB621CCb41023B437dBF14d94A7", //V1.0 (UUDP)
 };
 
 export default publicAddr;
