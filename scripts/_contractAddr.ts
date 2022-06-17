3df803abf4ab2f4c050ca59adc269ad2534ea197
Roy <toledoroy@gmail.com>
2022-06-17 07:33:29 -0700

    Deployed Updated C & J

--- scripts/_contractAddr.ts
+++ scripts/_contractAddr.ts
@@ -1,8 +1,8 @@
 //Track Addresses (Fill in present addresses to prevent new deplopyment)
 const contractAddr = {
     config:"0xA888d7f44638AEEBA9048Ca08bCF83a23e9f58dc",  //D2.1
-    jurisdiction:"0x6D9c0313Ab8D5C2b76711d201472E9d0F565d381",  //D2.9
-    case:"0xded92D349f97f969e435c6e6b417B541874FC46a",  //D2.6
+    jurisdiction:"0x756cD30224695e4253eD5e87031407A369168410",  //D3.0
+    case:"0x892BFd51dE73c2305d3C7218F3B45cFb7cCFA586",  //D3.0
     hub:"0xbf10eFEd01E280a4c21D11caE18404B67BF8bd78", //D4.5 (Proxy)
     avatar:"0xE253c1F803C1835325360a28eBDE83346f30cC38",  //D2.7 (Proxy)
     history:"0x9DFd9bc6600E6Ba1B14E7b6f2250D340936EB5B4", //D4.3 (Proxy)
