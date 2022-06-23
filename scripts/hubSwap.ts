0e414409fbb1125588edb801f37615b2e13ff28a
Roy <toledoroy@gmail.com>
2022-06-22 17:58:30 -0700

    Hub Migration Script

--- scripts/hubSwap.ts
+++ scripts/hubSwap.ts
@@ -16,10 +16,18 @@ let oldHubAddr = "0x288B2040e78dC90D73d8Ed0957ed706260DC8EfE";
  * Migrate Contracts Between Hubs
  */
 async function main() {
+  
     //Old Hub
     let oldHubContract = await ethers.getContractFactory("HubUpgradable").then(res => res.attach(oldHubAddr));
     //Move Asset Contracts to new Hub
     oldHubContract.hubChange(contractAddr.hub);
+
+    //New Hub
+    let hubContract = await ethers.getContractFactory("HubUpgradable").then(res => res.attach(contractAddr.hub));
+    //Set Contract Associations
+    await hubContract.setAssoc("avatar", contractAddr.avatar);
+    await hubContract.setAssoc("history", contractAddr.history);
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
