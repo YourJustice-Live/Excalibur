69d701c92a38d099dd7b2383b35e3a0f48a4433d
Roy <toledoroy@gmail.com>
2022-07-04 13:16:26 -0700

    Added Global String Match Support

--- contracts/abstract/CommonYJ.sol
+++ contracts/abstract/CommonYJ.sol
@@ -56,4 +56,9 @@ abstract contract CommonYJ is ICommonYJ, Ownable {
         _HUB = IHub(hubAddr);
     }
     
+    /// Match Two Strings
+    function _stringMatch(string memory str1, string memory str2) internal pure returns(bool){
+        return (keccak256(abi.encodePacked(str1)) == keccak256(abi.encodePacked(str2)));
+    }
+
 }
