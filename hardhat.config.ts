ff80cb73c1eb3ff8f936f99b29ba1cf077f13965
Roy <toledoroy@gmail.com>
2022-07-04 22:43:44 -0700

    [REFAC] Environment Variables

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -55,12 +55,12 @@ const config = {
       
     },
     goerli: {
-      url: process.env.ALCHEMY_GOERLI_URL || "",
+      url: process.env.GOERLI_RPC || "",
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
 
     mumbai: {
-      url: process.env.ALCHEMY_MUMBAI_URL || "",
+      url: process.env.MUMBAI_RPC || "",
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     polygon: {
@@ -80,11 +80,11 @@ const config = {
     
     
     optimism: {
-      url: process.env.ALCHEMY_OP_URL || "",
+      url: process.env.OP_RPC || "",
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     optimism_kovan: {
-      url: process.env.ALCHEMY_OP_TEST_URL || "",
+      url: process.env.OP_TEST_RPC || "",
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
 
