4a59a96eb827e96af9560a024ad243a18aa9d71b
Roy <toledoroy@gmail.com>
2022-07-05 09:04:48 -0700

    Copy of Multicall contract from TheGraph

---
+++ contracts/abstract/Multicall.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+pragma solidity 0.8.4;
+// pragma abicoder v2;
+
+import "../interfaces/IMulticall.sol";
+
+// Source: https://github.com/graphprotocol/contracts/blob/69620375eabcc3744f8985990e2356b9acd38ba8/contracts/base/Multicall.sol
+// Inspired by https://github.com/Uniswap/uniswap-v3-periphery/blob/main/contracts/base/Multicall.sol
+// Note: Removed payable from the multicall
+
+/**
+ * @title Multicall
+ * @notice Enables calling multiple methods in a single call to the contract
+ */
+abstract contract Multicall is IMulticall {
+    /// @inheritdoc IMulticall
+    function multicall(bytes[] calldata data) external override returns (bytes[] memory results) {
+        results = new bytes[](data.length);
+        for (uint256 i = 0; i < data.length; i++) {
+            (bool success, bytes memory result) = address(this).delegatecall(data[i]);
+
+            if (!success) {
+                // Next 5 lines from https://ethereum.stackexchange.com/a/83577
+                if (result.length < 68) revert();
+                assembly {
+                    result := add(result, 0x04)
+                }
+                revert(abi.decode(result, (string)));
+            }
+
+            results[i] = result;
+        }
+    }
+}
