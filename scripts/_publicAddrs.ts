04642b4543f826f65686741b44f072095de26286
kiv1n <aloha@kiv1n.ru>
2022-07-05 08:20:54 +0300

    Deployd contracts to mumbai

--- scripts/_publicAddrs.ts
+++ scripts/_publicAddrs.ts
@@ -1,18 +1,16 @@
-//Track Addresses (Fill in present addresses to user existing deplopyment)
+// Track Addresses (fill in present addresses to prevent new deplopyment)
+// TODO: Rename file to '_publicAddr.ts'
 const publicAddr: any = {
-    rinkeby:{
-        assocRepo: "0xfd5592B4604c5a422c507c4b116b51EE8e80E0C3", //V1.1
-        openRepo: "0x7b0AA37bCf5D231C13C920E0e372027919510fF9", //V2.0 (UUDP)
-    },
-    mumbai:{
-        openRepo: "",
-    },
-    optimism:{
-        openRepo: "",
-    },
-    optimism_kovan:{
-        openRepo: "0x8761b3E3bCDd243A063f18d5C24528C1400FA95B",
-    },
+  rinkeby: {
+    assocRepo: "0xfd5592B4604c5a422c507c4b116b51EE8e80E0C3", // V1.1
+    openRepo: "0x7b0AA37bCf5D231C13C920E0e372027919510fF9", // V2.0 (UUDP)
+  },
+  goerli: {},
+  mumbai: {
+    openRepo: "0x539dA825856778B593a55aC4E8A0Ec1441f18e78", // V2.0 (UUDP)
+  },
+  optimism: {},
+  optimism_kovan: {},
 };
 
 export default publicAddr;
