6b1a4485eb25ddbca687a88c82492f89b66a9e51
Roy <toledoroy@gmail.com>
2022-07-05 09:11:10 -0700

    Cleanup

--- contracts/interfaces/IPosts.sol
+++ contracts/interfaces/IPosts.sol
@@ -5,15 +5,11 @@ pragma solidity 0.8.4;
 
 interface IPosts {
 
-
     //--- Functions
-
 
     //--- Events
 
     /// General Post / Evidence, etc'
-    // event Post(address indexed account, string entRole, string postRole, string uri);        //postRole Moved to uri
-    // event Post(address indexed account, string entRole, string uri); //Added Caller Token ID
     event Post(address indexed account, uint256 tokenId, string entRole, string uri);
 
 }
