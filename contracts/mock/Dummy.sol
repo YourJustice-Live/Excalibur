1b35b00f64959419dc04bb5ffa6f9e498af8184b
Roy <toledoroy@gmail.com>
2022-07-06 15:45:35 -0700

    Dummy Contract (for testing purposes)

---
+++ contracts/mock/Dummy.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+
+/**
+ * @title Dummy Contract
+ */
+contract Dummy {
+
+    constructor() {
+
+    }
+
+
+    function debugFunc() public pure returns(string memory){
+        return "Hello World";
+    }
+
+
+}
